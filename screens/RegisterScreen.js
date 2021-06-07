4dd1b8ca89bbbcdd56a89eda0e271371b9773497
Mina <mina.latif@live.com>
2021-06-07 12:57:05 -0400

    Initial commit

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,81 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Button, Input, Text } from 'react-native-elements';
+
+const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: 'Login',
+    });
+  }, [navigation]);
+
+  const register = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create a new account!
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full Name"
+          autoFocus
+          type="text"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Profile Picture URL (optional)"
+          type="text"
+          value={imageUrl}
+          onChangeText={(text) => setImageUrl(text)}
+          onSubmitEditing={register}
+        />
+      </View>
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  inputContainer: { width: 400 },
+  button: {
+    width: 400,
+    marginTop: 20,
+  },
+});
