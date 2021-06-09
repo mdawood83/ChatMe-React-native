0f2548f041df2bd3171eecebfa94dfb3f73e65b2
Mina <mina.latif@live.com>
2021-06-09 18:55:48 -0400

    added AddChat & Screens

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import { db } from '../config/firebase';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+  const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('Chats')
+      .doc(id)
+      .collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) =>
+        setChatMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+
+    return unsubscribe;
+  });
+
   return (
     <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
       <Avatar
         rounded
         source={{
-          uri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+          uri:
+            chatMessages?.[0]?.photoURL ||
+            'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
         }}
       />
       <ListItem.Content>
@@ -16,7 +34,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
           {chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-          This is a test subtitle
+          {chatMessages?.[0]?.displayName}: {chatMessages?.[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -9,13 +9,19 @@ import {
   Platform,
   ScrollView,
   TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
+import firebase from 'firebase/app';
+import { db, auth } from '../config/firebase';
 
 const ChatScreen = ({ navigation, route }) => {
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Chat',
@@ -31,7 +37,9 @@ const ChatScreen = ({ navigation, route }) => {
           <Avatar
             rounded
             source={{
-              uri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+              uri:
+                messages[0]?.data.photoURL ||
+                'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
             }}
           />
           <Text style={{ color: '#FFF', marginLeft: 10, fontWeight: '700' }}>
@@ -65,7 +73,39 @@ const ChatScreen = ({ navigation, route }) => {
         </View>
       ),
     });
-  }, [navigation]);
+  }, [navigation, messages]);
+
+  const sendMessage = () => {
+    Keyboard.dismiss();
+
+    db.collection('Chats').doc(route.params.id).collection('messages').add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      displayName: auth.currentUser.displayName,
+      email: auth.currentUser.email,
+      photoURL: auth.currentUser.photoURL,
+    });
+
+    setInput('');
+  };
+
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection('Chats')
+      .doc(route.params.id)
+      .collection('messages')
+      .orderBy('timestamp', 'asc')
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    return unsubscribe;
+  }, [route]);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
       <StatusBar style="light" />
@@ -74,17 +114,71 @@ const ChatScreen = ({ navigation, route }) => {
         style={styles.container}
         keyboardVerticalOffset={90}
       >
-        <>
-          <ScrollView>{/* Chat goes here */}</ScrollView>
-          <View style={styles.footer}>
-            <TextInput
-              value={input}
-              onChangeText={(text) => setInput(text)}
-              placeholder="Your Message"
-              style={styles.textInput}
-            />
-          </View>
-        </>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+              {messages.map(({ id, data }) =>
+                data.email === auth.currentUser.email ? (
+                  <View key={id} style={styles.reciever}>
+                    <Avatar
+                      rounded
+                      // WEB
+                      containerStyle={{
+                        position: 'absolute',
+                        top: -17,
+                        left: -7,
+                      }}
+                      // IOS
+                      position="absolute"
+                      top={-15}
+                      left={-5}
+                      size={30}
+                      source={{
+                        uri: data.photoURL,
+                      }}
+                    />
+                    {/* <Text style={styles.recieverName}>{data.displayName}</Text> */}
+                    <Text style={styles.recieverText}>{data.message}</Text>
+                  </View>
+                ) : (
+                  <View key={id} style={styles.sender}>
+                    <Avatar
+                      rounded
+                      // WEB
+                      containerStyle={{
+                        position: 'absolute',
+                        top: -17,
+                        left: -7,
+                      }}
+                      // IOS
+                      position="absolute"
+                      top={-15}
+                      left={-5}
+                      size={30}
+                      source={{
+                        uri: data.photoURL,
+                      }}
+                    />
+                    <Text style={styles.senderName}>{data.displayName}</Text>
+                    <Text style={styles.senderText}>{data.message}</Text>
+                  </View>
+                )
+              )}
+            </ScrollView>
+            <View style={styles.footer}>
+              <TextInput
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={sendMessage}
+                placeholder="Type a message..."
+                style={styles.textInput}
+              />
+              <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                <Ionicons name="send" size={24} color="#2B68E6" />
+              </TouchableOpacity>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -93,7 +187,70 @@ const ChatScreen = ({ navigation, route }) => {
 export default ChatScreen;
 
 const styles = StyleSheet.create({
-  container: {},
-  footer: {},
-  textInput: {},
+  container: {
+    flex: 1,
+  },
+  reciever: {
+    padding: 15,
+    // backgroundColor: '#ECECEC',
+    backgroundColor: '#2B68E6',
+    // backgroundColor: '#ADD8E6',
+    alignSelf: 'flex-end',
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  sender: {
+    padding: 15,
+    // backgroundColor: '#2B68E6',
+    backgroundColor: '#ECECEC',
+    alignItems: 'flex-start',
+    borderRadius: 20,
+    margin: 15,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  senderName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: '#000',
+  },
+  senderText: {
+    color: '#000',
+    fontWeight: '500',
+    marginLeft: 10,
+    marginBottom: 15,
+  },
+  recieverName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: '#000',
+  },
+  recieverText: {
+    // color: '#00008b',
+    color: '#FFF',
+    fontWeight: '500',
+    marginLeft: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    padding: 15,
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 15,
+    borderColor: 'transparent',
+    backgroundColor: '#ECECEC',
+    padding: 10,
+    color: '#555',
+    borderRadius: 30,
+  },
 });
